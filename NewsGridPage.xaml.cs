7be6c9a7fa4e0b126ba54166b552afb3e17f90e8
LudvigOJonsson <40599505+LudvigOJonsson@users.noreply.github.com>
2018-08-08 11:27:47 +0200

    Now with Optimized Newsfeed

--- NewsGridPage.xaml.cs
+++ NewsGridPage.xaml.cs
@@ -9,23 +9,88 @@ using Xamarin.Forms.Xaml;
 
 namespace NWT
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NewsGridPage : ContentPage
 	{
         public static int Startnr = 1;
         public static int Stopnr = 1;
         public static int DBLN = 30;
         public static int NTN = DBLN / 5;
+        public static int Rownr = 1;
+        public static TapGestureRecognizer TGR = new TapGestureRecognizer();
+        public static List<Article> ArticleList = new List<Article>();
+        public static List<string> imageLinks = new List<string>();
+        public static Random rnd = new Random();
+        public static Button LoadNewsButton = new Button() {Text = "Load"};
+        public bool NWT = true;
+        public bool Mariestad = true;
+        public bool HJO = true;
+        public bool SLA = true;
 
-        public static List<KeyValuePair<BoxView, KeyValuePair<Label, Image>>> ArticleList = new List<KeyValuePair<BoxView, KeyValuePair<Label, Image>>>();
-        public List<string> imageLinks = new List<string>();
-        Random rnd = new Random();
+        public class Article
+        {
+            public int ID = 0;
+            public string Source = "";
+            public BoxView Box = new BoxView { };
+            public Label Label = new Label { };
+            public Image Image = new Image { };
 
+            public Article(RSSTable RSS)
+            {
+                Source = RSS.Source;
+                ID = RSS.ID;
+                Box = new BoxView
+                {
+                    Color = Color.FromHex("#FFFFFF"),
+                    WidthRequest = 200,
+                    HeightRequest = 80,
+                    HorizontalOptions = LayoutOptions.Fill,
+                    VerticalOptions = LayoutOptions.End,
+                    ClassId = RSS.Source
+                };
+
+                Label = new Label
+                {
+                    Text = RSS.Title,
+                    HorizontalTextAlignment = TextAlignment.Start,
+                    VerticalTextAlignment = TextAlignment.End,
+                    HeightRequest = 80,
+                    TextColor = Color.Black,
+                    FontSize = 25,
+                    ClassId = RSS.ID.ToString(),
+                    Margin = 10
+                };
+
+                Label.GestureRecognizers.Add(TGR);
+
+                Image = new Image
+                {
+                    Source = imageLinks[rnd.Next(7)],
+                    WidthRequest = 200,
+                    HeightRequest = 300,
+                    Aspect = Aspect.AspectFill
+                };
+            }
+
+            public void Visibility(bool State){
+
+                Image.IsVisible = State;
+                Label.IsVisible = State;
+                Box.IsVisible = State;
+            }
+
+
+
+
+        }
 
 
         public NewsGridPage ()
 		{
 			InitializeComponent ();
+
             imageLinks.Add("http://media2.hitzfm.nu/2016/11/Nyheter_3472x1074.jpg");
             imageLinks.Add("https://pbs.twimg.com/media/CynmmdYWgAAjky1.jpg");
             imageLinks.Add("https://www.surfertoday.com/images/stories/clouds.jpg");
@@ -34,6 +99,17 @@ namespace NWT
             imageLinks.Add("https://cdn2.acsi.eu/5/8/5/2/5852b667270eb.jpeg");
             imageLinks.Add("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Runder_Berg.JPG/1200px-Runder_Berg.JPG");
             imageLinks.Add("https://thumbs.dreamstime.com/z/online-robber-17098197.jpg");
+
+            TGR.NumberOfTapsRequired = 1;
+
+            TGR.Tapped += (s, e) => {
+                LoadNews(s, e);
+            };
+
+            LoadNewsButton.Clicked += (s, e) => {
+                AddNews();
+            };
+
             AddNews();
             //NewsButtonN.Image = ImageSource.FromFile("newsfeed.png");
         }
@@ -47,151 +123,32 @@ namespace NWT
 
         public void PrintNews()
         {
-            int X = 1;
-            bool NWT = true;
-            bool Mariestad = true;
-            bool HJO = true;
-            bool SLA = true;
+            
 
-
+            
             if (App.Instanciated)
             {
                 NWT = App.SideMenu.NWT.IsToggled;
                 Mariestad = App.SideMenu.Mariestad.IsToggled;
                 HJO = App.SideMenu.HJO.IsToggled;
                 SLA = App.SideMenu.SLA.IsToggled;
-
             }
 
 
-
-            var TGR = new TapGestureRecognizer();
-
-            TGR.Tapped += (s, e) => {
-                LoadNews(s, e);
-            };
-            TGR.NumberOfTapsRequired = 1;
-
-            int ColourNR = 1;
-
-            Color Colour = Color.Black;
-
-            var Rss = App.database.GetRSS(Stopnr);
-            Console.WriteLine(Rss.Count);
-            ArticleList.Clear();
-
-            foreach (RSSTable RSS in Rss)
+            foreach (var Article in ArticleList)
             {
-                switch (ColourNR)
+                if ((Article.Source == "NWT" && NWT) ||
+                   (Article.Source == "Mariestad" && Mariestad) ||
+                   (Article.Source == "Hjo" && HJO) ||
+                   (Article.Source == "SLA" && SLA))
                 {
-                    case 1:
-                        Colour = Color.Red;
-                        ColourNR++;
-                        break;
-
-                    case 2:
-                        Colour = Color.Orange;
-                        ColourNR++;
-                        break;
-
-                    case 3:
-                        Colour = Color.Yellow;
-                        ColourNR++;
-                        break;
-
-                    case 4:
-                        Colour = Color.Green;
-                        ColourNR++;
-                        break;
-
-                    case 5:
-                        Colour = Color.Blue;
-                        ColourNR++;
-                        break;
-
-                    case 6:
-                        Colour = Color.Purple;
-                        ColourNR = 1;
-                        break;
-
+                    Article.Visibility(true);
                 }
-
-
-
-                if ((RSS.Source == "NWT" && NWT) ||
-                   (RSS.Source == "Mariestad" && Mariestad) ||
-                   (RSS.Source == "Hjo" && HJO) ||
-                   (RSS.Source == "SLA" && SLA))
+                else
                 {
-
-                    NewsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-
-                    var boxview = new BoxView { };
-                    boxview = new BoxView
-                    {
-                        Color = Color.FromHex("#FFFFFF"),
-                        WidthRequest = 200,
-                        HeightRequest = 80,
-                        HorizontalOptions = LayoutOptions.Fill,
-                        VerticalOptions = LayoutOptions.End,
-                        ClassId = RSS.Source
-                    };
-
-                    var label = new Label { };
-                    label = new Label
-                    {
-                        Text = RSS.Title,
-                        HorizontalTextAlignment = TextAlignment.Start,
-                        VerticalTextAlignment = TextAlignment.End,
-                        HeightRequest = 80,
-                        TextColor = Colour,
-                        FontSize = 25,
-                        ClassId = RSS.ID.ToString(),
-                        Margin = 10
-                    };
-
-                    var image = new Image { };
-
-                    image = new Image
-                    {
-                        Source = imageLinks[rnd.Next(7)],
-                        WidthRequest = 200,
-                        HeightRequest = 300,
-                        Aspect = Aspect.AspectFill
-                    };
-
-                    label.GestureRecognizers.Add(TGR);
-                    var SubArticle = new KeyValuePair<Label, Image>(label, image);
-                    var Article = new KeyValuePair<BoxView, KeyValuePair<Label, Image>>(boxview, SubArticle);
-                    ArticleList.Add(Article);
-
+                    Article.Visibility(false);
                 }
             }
-            NewsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-
-
-            NewsGrid.Children.Clear();
-
-            var LoadNewsButton = new Button()
-            {
-                Text = "Load",
-            };
-
-            LoadNewsButton.Clicked += (s, e) => {
-                AddNews();
-            };
-
-            foreach (var s in ArticleList)
-            {
-                NewsGrid.RowSpacing = 0;
-                NewsGrid.Children.Add(s.Value.Value, 0, X - 1); //Image
-
-                NewsGrid.Children.Add(s.Key, 0, X - 1); //Boxview
-                NewsGrid.Children.Add(s.Value.Key, 0, X - 1); //Label            
-                X++;
-            }
-            NewsGrid.Children.Add(LoadNewsButton, 0, X - 1);
-
         }
 
         public void AddNews()
@@ -201,8 +158,36 @@ namespace NWT
             {
                 FillLocalDB();
             }
-
             Stopnr += NTN;
+            var Rss = App.database.GetRSS(Stopnr);
+            Console.WriteLine(Rss.Count);
+
+            foreach (RSSTable RSS in Rss)
+            {
+                bool Exists = false;
+
+                foreach (var Article in ArticleList)
+                {
+                    if (Article.ID == RSS.ID)
+                    {
+                        Exists = true;
+                    }
+                }
+
+                if (!Exists)
+                {
+                    var Box = new Article(RSS);
+                    ArticleList.Add(Box);
+                    NewsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+                    NewsGrid.RowSpacing = 0;
+                    NewsGrid.Children.Add(Box.Image, 0, Rownr - 1); //Image
+                    NewsGrid.Children.Add(Box.Box, 0, Rownr - 1); //Boxview
+                    NewsGrid.Children.Add(Box.Label, 0, Rownr - 1); //Label
+                    Rownr++;
+                }
+            }
+            NewsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            NewsGrid.Children.Add(LoadNewsButton, 0, Rownr - 1);                    
             PrintNews();
 
         }
