2f0e1c5e45452aba792bb407b64f4b8f7afecef7
LudvigOJonsson <40599505+LudvigOJonsson@users.noreply.github.com>
2018-09-18 14:54:04 +0200

    Fullt fungerande CommentPage

--- CommentPage.xaml.cs
+++ CommentPage.xaml.cs
@@ -15,14 +15,26 @@ namespace NWT
         public int ArticleNR;
         public int CLVL = 1;
         public CommentTable root;
+        public List<CommentTable> Chain = new List<CommentTable>();
+        public Button BackButton = new Button() { Image = "uparrow.png" };
         public CommentPage (int argc,CommentTable s)
 		{
             root = s;
             ArticleNR = argc;
 			InitializeComponent ();
+
+            BackButton.Clicked += (o, e) => {
+                Chain.Remove(Chain.Last());
+                CLVL--;
+                LoadComments(CLVL, Chain.Last().ID);
+            };
+
             
 
-            LoadComments(CLVL);
+
+            Chain.Add(root);
+            LoadComments(CLVL,Chain.Last().ID);
+            
 		}
 
         void AddComment(CommentTable s)
@@ -131,18 +143,7 @@ namespace NWT
                 Margin = 0,
                 ClassId = "false"
             };
-            var Replies = new Button()
-            {
-                Text = "X Replies",
-                BackgroundColor = Color.FromRgb(80, 210, 194),
-                TextColor = Color.Black,
-                WidthRequest = 60,
-                HeightRequest = 20,
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.End,
-                Margin = 0,
-                
-            };
+            
             var Reply = new Button()
             {
                 BackgroundColor = Color.FromHex("#50d2c2"),
@@ -223,7 +224,7 @@ namespace NWT
                     }
 
                     App.database.PointUpdate(s, UV.Point);
-                    LoadComments(CLVL);
+                    LoadComments(CLVL, Chain.Last().ID);
 
                 }
 
@@ -267,13 +268,18 @@ namespace NWT
                     }
 
                     App.database.PointUpdate(s, UV.Point);
-                    LoadComments(CLVL);
+                    LoadComments(CLVL, Chain.Last().ID);
 
                 }
             };
             Reply.Clicked +=  (o, e) => {
                 SubmitComment(s.ID);
             };
+            
+
+            
+
+
             async void UpClicked(object sender, System.EventArgs e)
             {
                 await VoteArrowUp.RotateTo(-15, 80, Easing.BounceOut);
@@ -294,11 +300,42 @@ namespace NWT
             CommentGrid.Children.Add(Username, 1, 5, s.CommentNR, s.CommentNR + 1);
             CommentGrid.Children.Add(VoteArrowDown, 0, 1, s.CommentNR, s.CommentNR + 1);
             CommentGrid.Children.Add(VoteArrowUp, 0, 1, s.CommentNR, s.CommentNR + 1);
-            CommentGrid.Children.Add(VoteNumber, 0, 1, s.CommentNR, s.CommentNR + 1);
-            CommentGrid.Children.Add(Replies, 3, 4, s.CommentNR, s.CommentNR + 1);
+            CommentGrid.Children.Add(VoteNumber, 0, 1, s.CommentNR, s.CommentNR + 1);            
             CommentGrid.Children.Add(Reply, 5, 6, s.CommentNR, s.CommentNR + 1);
             CommentGrid.Children.Add(CurLVL, 4, 5, s.CommentNR, s.CommentNR + 1);
-            //CommentGrid.Children.Add(Userimage, 0, s.CommentNR + 8);        
+            //CommentGrid.Children.Add(Userimage, 0, s.CommentNR + 8);      
+
+            if (s.Replylvl == CLVL)
+            {
+                var Replies = new Button()
+                {
+                    Text = "X Replies",
+                    BackgroundColor = Color.FromRgb(80, 210, 194),
+                    TextColor = Color.Black,
+                    WidthRequest = 60,
+                    HeightRequest = 20,
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.End,
+                    Margin = 0,
+
+                };
+
+                Replies.Clicked += (o, e) => {
+                    Chain.Add(s);
+                    CLVL++;
+                    LoadComments(CLVL, Chain.Last().ID);
+                };
+
+                CommentGrid.Children.Add(Replies, 3, 4, s.CommentNR, s.CommentNR + 1);
+            }
+
+            if (CLVL > 1)
+            {
+                CommentGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+                CommentGrid.Children.Add(BackButton, 0, 6, s.CommentNR + 1, s.CommentNR + 2);
+            }
+
+
         }
 
         void SubmitComment(int ReplyNR)
@@ -311,7 +348,7 @@ namespace NWT
 
                 SC.Article = ArticleNR;
                 SC.CommentNR = CNR;
-                SC.UserSubmitted = 1;
+                SC.UserSubmitted = root.UserSubmitted;
                 SC.User = App.LoggedinUser.ID;
                 SC.Replynr = ReplyNR;
                 if (ReplyNR > -1)
@@ -330,7 +367,7 @@ namespace NWT
                 SC.Point = 0;
                 Comment.Text = "";
                 App.database.InsertComment(SC);
-                LoadComments(CLVL);
+                LoadComments(CLVL, Chain.Last().ID);
             }
             if (App.LoggedinUser != null)
             {
@@ -338,16 +375,22 @@ namespace NWT
             }
         }
 
-        void LoadComments(int LVL)
+        void LoadComments(int LVL, int ReplyID)
         {
-            var Query = App.database.GetComments(ArticleNR,LVL,root.ID);
-            
+            var Query = App.database.GetComments(ArticleNR,LVL,ReplyID);
             CommentGrid.Children.Clear();
-            AddComment(root);
+            foreach (var a in Chain)
+            {
+                AddComment(a);
+                
+            }           
             foreach (var s in Query)
             {
                 AddComment(s);   
             }
+
+            
+            
         }
     }
 }
