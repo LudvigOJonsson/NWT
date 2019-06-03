66bfde9d300ccef1e5bd288d9ba87f82d5948833
ChrisC <chris_7722@hotmail.com>
2019-06-03 13:59:36 +0200

    CustomizationPage
    :jp:

---
+++ CustomizationPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NWT
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CustomizationPage : ContentPage
+	{
+		public CustomizationPage()
+		{
+			InitializeComponent();
+            
+		}
+
+        public void FollowButton(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Text == "Follow")
+            {
+                button.Text = "Unfollow";
+            } else if (button.Text == "Unfollow")
+            {
+                button.Text = "Unfollow";
+            }
+        }
+    }
+}
